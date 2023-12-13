0e313752cc3c8c1acdfb78ef85c8e8b3911a76dc
salmatamernabil <104529836+salmatamernabil@users.noreply.github.com>
2023-12-13 11:08:11 +0200

    Update peer.py

--- client/peer.py
+++ client/peer.py
@@ -8,6 +8,9 @@ class Peer:
     self.serverPort = 5050
     self.tcpSocket = socket(AF_INET, SOCK_STREAM)
     self.tcpSocket.connect((self.serverIP, self.serverPort))
+    self.udpSocket = socket(AF_INET, SOCK_DGRAM)
+    self.udpSocket.bind(('localhost', 0)) 
+    self.start_hello_thread()
   
   def createAccount(self, username, password):
     message = "JOIN " + username + " " + password
@@ -17,4 +20,21 @@ class Peer:
       print("Failed. Account Already Exist :( ")
     elif response == "join-success":
       print("Account Created Successfully :) ")
+
+      def start_hello_thread(self):
+        hello_thread = threading.Thread(target=self.send_hello_message)
+        hello_thread.start()
+
+    def send_hello_message(self):
+        while True:
+            try:
+                message = "HELLO"
+                self.udpSocket.sendto(message.encode(), (self.serverIP, self.serverPort))
+                time.sleep(1)
+            except socket.error as e:
+                print(f"Error sending 'HELLO' message: {e}")
+                break
+
+
+
     
