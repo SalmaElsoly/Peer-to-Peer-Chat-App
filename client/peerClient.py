aceb614bf56320e753446c9cef4eaf957851846c
Salma Elsoly <salmaelsoly@gmail.com>
2023-12-28 04:47:59 +0200

    peer to peer arch first implementation

--- client/peerClient.py
+++ client/peerClient.py
@@ -1,18 +1,49 @@
 import threading
 import socket
 
+# this will be the thread that will handle the peer to peer connection of one to one chat
+
 
 class PeerClient(threading.Thread):
-    def __init__(self,username, chatRoomUsers):
+    def __init__(
+        self, username, peerToConnectIp, peerToConnectPort, peerServer, responseReceived
+    ):
         threading.Thread.__init__(self)
-        self.running = True
         self.username = username
-        self.chatRoomUsers = chatRoomUsers
-        self.udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.peerToConnectIp = peerToConnectIp
+        self.peerToConnectPort = peerToConnectPort
+        self.peerServer = peerServer
+        self.responseReceived = responseReceived
+        self.tcpClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.isInChat = False
 
     def run(self):
-        print("PeerClient thread started")
-   
-    def stop(self):
-        self.running = False
-        self.udpSocket.close()
+        print("Peer client started...")
+        try:
+            self.tcpClientSocket.connect((self.peerToConnectIp, self.peerToConnectPort))
+            if self.peerServer.isChatRequested == 0 and self.responseReceived is None:
+                requestMessage = (
+                    "CHAT-REQUEST "
+                    + str(self.peerServer.peerServerPort)
+                    + " "
+                    + self.username
+                )
+                self.tcpClientSocket.send(requestMessage.encode())
+                print("Request message " + requestMessage + " is sent...")
+                self.responseReceived = self.tcpClientSocket.recv(1024).decode()
+                print("Response message " + self.responseReceived + " is received...")
+                self.responseReceived = self.responseReceived.split()
+                if self.responseReceived[0] == "CHAT-ACCEPTED":
+                    self.peerServer.isChatRequested = 1
+                    self.peerServer.connectedPeerSocket = self.tcpClientSocket
+                    self.peerServer.connectedPeerIP = self.peerToConnectIp
+                    self.peerServer.connectedPeerPort = self.peerToConnectPort
+                    self.peerServer.chattingClientName = self.responseReceived[1]
+                    self.isInChat = True
+                    print("Chat is accepted...")
+                else:
+                    self.tcpClientSocket.close()
+                    print("Chat is not accepted...")
+        except socket.error:
+            print("Peer is not online...")
+            self.tcpClientSocket.close()
--- client/peerServer.py
+++ client/peerServer.py
@@ -1,22 +1,39 @@
 import socket
 import threading
 import select
-
+import logging
 
 class PeerServer(threading.Thread):
     def __init__(self, username, peerServerPort):
         threading.Thread.__init__(self)
         self.username = username
-        self.peerServerPort = peerServerPort
         self.tcpServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.peerServerIP = None
-        self.udpServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.peerServerPort = peerServerPort
+        self.isChatRequested = 0
+        self.connectedPeerSocket = None
+        self.connectedPeerIP = None
+        self.connectedPeerPort = None
+        self.isOnline = True
+        self.chattingClientName = None
 
     def run(self):
-        self.peerServerIP = socket.gethostbyname(socket.gethostname())
-        self.tcpServerSocket.bind((self.peerServerIP, self.peerServerPort))
-        self.tcpServerSocket.listen(5)
-        self.udpServerSocket.bind((self.peerServerIP, self.peerServerPort))
-        self.udpServerSocket.setblocking(0)
-        sockets = [self.tcpServerSocket]
-        
+        print("Peer server started...")
+        self.tcpServerSocket.bind(("", self.peerServerPort))
+        self.tcpServerSocket.listen(1)
+        inputs = [self.tcpServerSocket]
+        while inputs and self.isOnline:
+            try:
+                readable, writable, exceptional = select.select(
+                    inputs, [], inputs
+                )
+                for sock in readable:
+                    if sock is self.tcpServerSocket:
+                        clientSocket, clientAddress = self.tcpServerSocket.accept()
+                        clientSocket.setblocking(0)
+                        inputs.append(clientSocket)
+                        print("Peer client is connected...")
+            except OSError as oErr:
+                logging.error("OSError: {0}".format(oErr))
+            except ValueError as vErr:
+                logging.error("ValueError: {0}".format(vErr))
+                    
